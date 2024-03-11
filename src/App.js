80b976c8f171bea6c7c73a58551fbbdf19c9bf3e
NuriyevaJariyat <jariyatnuriyeva@gmail.com>
2024-03-11 11:08:36 +0400

    qwertyui

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header  className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and
-        </p>
-      </header>
-    </div>
+   <></>
   );
 }
 
