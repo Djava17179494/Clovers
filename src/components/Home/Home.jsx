6f2767d2b43f9169b86aedfec8df91b91f0a5337
aydanism <adnismmm@gmail.com>
2024-03-11 11:10:53 +0300

    change style

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+let Home = () =>{
+    return(
+        <>
+        <div className="">
+            <h1>Most Popular Categories</h1>
+            <div>
+                <img alt=""></img>
+                <p>Vegatables</p>
+            </div>
+        </div>
+        <div></div>
+        <div></div>
+        </>
+    )
+}
+export default Home;
