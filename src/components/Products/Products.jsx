84b69385b7947b87d3b67ac79fb7a68ec56e4355
abbsvsbnm <abbsvasbnm@gmail.com>
2024-03-11 12:11:00 +0400

    blablabla

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,2 +1,21 @@
 import React from "react";
 import './Products.css';
+
+const Product = () => {
+    return (
+      <div className="card">
+        <img
+          className="card-image"
+          src=""
+          alt=""
+        />
+        <div className="card-content">
+          <h2 className="card-title">Hand Cream</h2>
+          <p className="card-price">$10.99</p>
+          <button className="card-button">Add to Cart</button>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Product;
