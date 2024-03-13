fe821df662ecaea41ec31e5319000386c94cce7d
ElvinaSafarova <safarova_elvina@icloud.com>
2024-03-13 10:15:31 +0400

    before accardeon

--- src/elements/Counter.jsx
+++ src/elements/Counter.jsx
@@ -1,20 +1,28 @@
 import "./Counter.css";
-
-
+import React, { useState } from 'react';
 
 let Counter = () => {
-return (
-               <label>
-                    Quantity
-                    <div className="all-input" >
-                    <input type="number" className="productCount"/>
-                    <div className="plus-minus">
-                        <p className="plus">+</p>
-                        <p className="minus">-</p>
-                    </div>
-                    </div>
-                </label>
-)
+    const [count, setCount] = useState(0);
+  
+    const increment = () => {
+      setCount(count + 1);
+    };
+  
+    const decrement = () => {
+      setCount(count - 1);
+    };
+    return (
+        <label>
+            Quantity
+            <div className="all-input" >
+                <input type="text" className="productCount" value={count} />
+                <div className="plus-minus">
+                    <p className="plus" onClick={()=> increment()}>+</p>
+                    <p className="minus" onClick={()=> decrement()}>-</p>
+                </div>
+            </div>
+        </label>
+    )
 }
 
 export default Counter;
